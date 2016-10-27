5eba15fd473dfd575254c28acb512207b73ea381
cferdinandi <chris@gomakethings.com>
2016-10-27 10:41:44 -0400

    v4.4.1 Fixed ready state to account for complete

--- src/js/right-height.js
+++ src/js/right-height.js
@@ -110,7 +110,7 @@
 		if ( typeof fn !== 'function' ) return;
 
 		// If document is already loaded, run method
-		if ( document.readyState === 'interactive'  ) {
+		if ( document.readyState === 'interactive' || document.readyState === 'complete' ) {
 			return fn();
 		}
 
